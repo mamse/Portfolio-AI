698a3beb35626f1634521e19737bdfbef3dc4757
Migmartins <migmartins@pictet-technologies.com>
2023-11-19 22:32:25 +0000

    update salary component

--- src/app/salary-calculator/salary-calculator.component.ts
+++ src/app/salary-calculator/salary-calculator.component.ts
@@ -10,21 +10,32 @@ export class SalaryCalculatorComponent {
   monthlySalary = 0;
   yachtTime: string | undefined;
   islandTime: string | undefined;
+  iphoneTime: string | undefined;
+  ferrariTime: string | undefined;
 
   calculateTimes() {
+    this.iphoneTime = this.calculateTimeToAfford(1000);
+    this.ferrariTime = this.calculateTimeToAfford(500000);
     this.yachtTime = this.calculateTimeToAfford(1000000);
     this.islandTime = this.calculateTimeToAfford(10000000);
   }
 
   private calculateTimeToAfford(price: number): string {
-    const months = Math.ceil(price / this.monthlySalary);
+    const months = Math.floor(price / this.monthlySalary);
     const years = Math.floor(months / 12);
     const remainingMonths = months % 12;
+    const days = Math.floor((price / this.monthlySalary) * 30); // Assuming an average month has 30 days
 
-    const yearString = years > 0 ? `${years} year${years > 1 ? 's' : ''}` : '';
-    const monthString = remainingMonths > 0 ? `${remainingMonths} month${remainingMonths > 1 ? 's' : ''}` : '';
-
-    return `${yearString}${yearString && monthString ? ' and ' : ''}${monthString}`;
+    if (years >= 1) {
+      const yearString = years > 0 ? `${years} year${years > 1 ? 's' : ''}` : '';
+      const monthString = remainingMonths > 0 ? `${remainingMonths} month${remainingMonths > 1 ? 's' : ''}` : '';
+      return `${yearString}${yearString && monthString ? ' and ' : ''}${monthString}`;
+    } else if (remainingMonths >= 1) {
+      return `${remainingMonths} month${remainingMonths > 1 ? 's' : ''}`;
+    } else {
+      return `${days} day${days > 1 ? 's' : ''}`;
+    }
   }
 }
+
 
